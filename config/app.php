49a254ed821fa179d85404083e4f0d8af49cf9f1
Richard.CW.Ting <richard.cw.ting@benq.com>
2016-08-08 18:26:58 +0800

    Add 'laravelcollective/html' package

--- config/app.php
+++ config/app.php
@@ -153,6 +153,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Collective\Html\HtmlServiceProvider::class,
 
 
         /*
@@ -208,6 +209,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
