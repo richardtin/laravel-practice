7cac85359961b22f636d8f9769395892b818622a
Richard.CW.Ting <richard.cw.ting@benq.com>
2016-08-15 18:02:36 +0800

    Re-layout UI as table view

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -16,6 +16,14 @@
     <!-- Custom Fonts -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
+    <style>
+        .table > tbody > tr > td {
+            vertical-align: middle;
+        }
+        .table-borderless tbody tr td, .table-borderless tbody tr th, .table-borderless thead tr th {
+            border: none;
+        }
+    </style>
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -28,161 +36,159 @@
 <body>
 
     <div class="container-fluid">
-        <form class="form-inline">
-        <fieldset disabled>
-            <div class="form-group">
-                <label for="labelRoomHeight">H:</label>
-                <input type="text" class="form-control" id="labelRoomHeight" placeholder="100.0">
-            </div>
-            <div class="form-group">
-                <label for="labelRoomWidth">W:</label>
-                <input type="email" class="form-control" id="labelRoomWidth" placeholder="100.0">
-            </div>
-            <div class="form-group">
-                <label for="labelRoomLength">L:</label>
-                <input type="email" class="form-control" id="labelRoomLength" placeholder="100.0">
-            </div>
-        </fieldset>
-        </form>
-        <p>
-        <form class="form-inline">
-            <div class="form-group">
-                <label class="sr-only" for="exampleInputHeight">Height</label>
-                <div class="input-group col-sm-2">
-                    <div class="input-group-addon">H:</div>
-                    <input type="text" class="form-control" id="exampleInputHeight" placeholder="100.0">
-                </div>
-                <label class="sr-only" for="exampleInputWidth">Width</label>
-                <div class="input-group col-sm-2">
-                    <div class="input-group-addon">W:</div>
-                    <input type="text" class="form-control" id="exampleInputWidth" placeholder="100.0">
-                </div>
-                <label class="sr-only" for="exampleInputLength">Length</label>
-                <div class="input-group col-sm-2">
-                    <div class="input-group-addon">L:</div>
-                    <input type="text" class="form-control" id="exampleInputLength" placeholder="100.0">
-                </div>
-            </div>
-        </form>
-        <p>
-        <form class="form-horizontal">
-            <div class="form-group">
-                <label for="inputModelName" class="col-sm-2 control-label">Model Type:</label>
-                <!-- Single button -->
-                <div class="btn-group col-sm-2">
-                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Type 1 <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Type 1</a></li>
-                        <li><a href="#">Type 2</a></li>
-                        <li><a href="#">Type 3</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Type 4</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="inputModelName" class="col-sm-2 control-label">Model Name:</label>
-                <!-- Single button -->
-                <div class="btn-group col-sm-2">
-                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        DW883UST <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">DW883UST</a></li>
-                        <li><a href="#">Name 2</a></li>
-                        <li><a href="#">Name 3</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">Name 4</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="inputLense" class="col-sm-2 control-label">Lense:</label>
-                <!-- Single button -->
-                <div class="btn-group col-sm-2">
-                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        0.23 <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">0.23</a></li>
-                        <li><a href="#">0.24</a></li>
-                        <li><a href="#">0.25</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li><a href="#">0.26</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="form-group">
-                <label for="inputAspectRatio" class="col-sm-2 control-label">Aspect Ratio:</label>
-                <!-- Single button -->
-                <div class="btn-group col-sm-2">
-                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        16:10 <span class="caret"></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">16:10</a></li>
-                        <li><a href="#">16:9</a></li>
-                        <li><a href="#">4:3</a></li>
-                    </ul>
-                </div>
-            </div>
-        </form>
-        <div class="row"><div class="col-sm-3">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Screen Size
-            </div>
-            <div class="panel-body">
-                <form class="form-horizontal">
-                    <div class="form-group">
-                        <label class="sr-only" for="screenDiagnoal">Diagnoal</label>
-                        <div class="input-group col-sm-10">
-                            <div class="input-group-addon">Diagnoal:</div>
-                                <input type="text" class="form-control" id="screenDiagnoal" placeholder="86.0">
-                            <div class="input-group-addon">inch</div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="sr-only" for="screenWidth">Width</label>
-                        <div class="input-group col-sm-10">
-                            <div class="input-group-addon">Width:</div>
-                                <input type="text" class="form-control" id="screenWidth" placeholder="72.9">
-                            <div class="input-group-addon">inch</div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="sr-only" for="screenHeight">Height</label>
-                        <div class="input-group">
-                            <div class="input-group-addon">Height:</div>
-                                <input type="text" class="form-control" id="screenHeight" placeholder="45.6">
-                            <div class="input-group-addon">inch</div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-        </div></div>
 
         <div class="row"><div class="col-sm-3">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Projector to screen
-            </div>
-            <div class="panel-body">
-                <form class="form-horizontal">
-                    <div class="form-group">
-                        <label class="sr-only" for="screenDistance">Distance</label>
-                        <div class="input-group">
-                            <div class="input-group-addon">Distance:</div>
-                                <input type="text" class="form-control" id="screenDistance" placeholder="72.8">
-                            <div class="input-group-addon">inch</div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
+            <table class="table table-borderless">
+                <tr class="success"><td>Room Dimension</td></tr>
+                <tr><td>
+                    <table class="table table-condensed">
+                        <tr>
+                            <td>
+                                <label class="sr-only" for="exampleInputHeight">Height</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">H:</div>
+                                    <input type="text" class="form-control" id="exampleInputHeight" placeholder="100.0">
+                                </div>
+                            </td>
+                            <td>
+                                <label class="sr-only" for="exampleInputWidth">Width</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">W:</div>
+                                    <input type="text" class="form-control" id="exampleInputWidth" placeholder="100.0">
+                                </div>
+                            </td>
+                            <td>
+                                <label class="sr-only" for="exampleInputLength">Length</label>
+                                <div class="input-group">
+                                    <div class="input-group-addon">L:</div>
+                                    <input type="text" class="form-control" id="exampleInputLength" placeholder="100.0">
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td></tr>
+                <tr class="success"><td>Projector</td></tr>
+                <tr><td>
+                    <table class="table table-condensed table-borderless">
+                        <tr>
+                            <td>Model Type:</td>
+                            <td>
+                                <div class="btn-group col-sm-12">
+                                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Type 1 <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Type 1</a></li>
+                                        <li><a href="#">Type 2</a></li>
+                                        <li><a href="#">Type 3</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="#">Type 4</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Model Name:</td>
+                            <td>
+                                <div class="btn-group col-sm-12">
+                                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        DW883UST <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">DW883UST</a></li>
+                                        <li><a href="#">Name 2</a></li>
+                                        <li><a href="#">Name 3</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="#">Name 4</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Lense:</td>
+                            <td>
+                                <div class="btn-group col-sm-12">
+                                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        0.23 <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">0.23</a></li>
+                                        <li><a href="#">0.24</a></li>
+                                        <li><a href="#">0.25</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a href="#">0.26</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Aspect Ratio:</td>
+                            <td>
+                                <div class="btn-group col-sm-12">
+                                    <button type="button" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        16:10 <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">16:10</a></li>
+                                        <li><a href="#">16:9</a></li>
+                                        <li><a href="#">4:3</a></li>
+                                    </ul>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td></tr>
+                <tr class="success"><td>Screen Size</td></tr>
+                <tr><td>
+                    <table class="table table-condensed">
+                        <tr>
+                            <td>Diagnoal:</td>
+                            <td>
+                                <label class="sr-only" for="screenDiagnoal">Diagnoal</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="screenDiagnoal" placeholder="86.0">
+                                    <div class="input-group-addon">inch</div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Width:</td>
+                            <td>
+                                <label class="sr-only" for="screenWidth">Width</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="screenWidth" placeholder="72.9">
+                                    <div class="input-group-addon">inch</div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Height:</td>
+                            <td>
+                                <label class="sr-only" for="screenHeight">Height</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="screenHeight" placeholder="45.6">
+                                    <div class="input-group-addon">inch</div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td></tr>
+                <tr class="success"><td>Projector to Screen</td></tr>
+                <tr><td>
+                    <table class="table table-condensed">
+                        <tr>
+                            <td>Distance:</td>
+                            <td>
+                                <label class="sr-only" for="screenDistance">Distance</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control" id="screenDistance" placeholder="72.8">
+                                    <div class="input-group-addon">inch</div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </td></tr>
+            </table>
         </div></div>
 
     </div>
